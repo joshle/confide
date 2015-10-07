84887388cddbcfc5b61de5fd2e0be0b350416ee7
einice <einice@vip.qq.com>
2015-10-08 02:54:26 +0800

    Update passwordreset.blade.php

--- src/views/emails/passwordreset.blade.php
+++ src/views/emails/passwordreset.blade.php
@@ -1,10 +1,10 @@
-<h1>{{ Lang::get('confide::confide.email.password_reset.subject') }}</h1>
+<h1>{{ trans('confide::confide.email.password_reset.subject') }}</h1>
 
-<p>{{ Lang::get('confide::confide.email.password_reset.greetings', array( 'name' => (isset($user['username'])) ? $user['username'] : $user['email'])) }},</p>
+<p>{{ trans('confide::confide.email.password_reset.greetings', array( 'name' => (isset($user['username'])) ? $user['username'] : $user['email'])) }},</p>
 
-<p>{{ Lang::get('confide::confide.email.password_reset.body') }}</p>
-<a href='{{ URL::to('users/reset_password/'.$token) }}'>
-    {{ URL::to('users/reset_password/'.$token)  }}
+<p>{{ trans('confide::confide.email.password_reset.body') }}</p>
+<a href='{{ url('users/reset_password/'.$token) }}'>
+    {{ url('users/reset_password/'.$token)  }}
 </a>
 
-<p>{{ Lang::get('confide::confide.email.password_reset.farewell') }}</p>
+<p>{{ trans('confide::confide.email.password_reset.farewell') }}</p>
